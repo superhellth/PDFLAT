e4c34e7ed1219536b3ba5283effa728a68a891a1
Nico Hellthaler <nico.hellthaler@web.de>
2023-12-04 11:01:30 +0100

    Add enrichment

--- api/api/analysis/data_analysis.py
+++ api/api/analysis/data_analysis.py
@@ -1,19 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import yake
-from rank_bm25 import BM25Okapi
 from sklearn.manifold import TSNE
 import pandas as pd
 import seaborn as sns
 from api.analysis.data_provider import DataProvider
 from api.analysis.resolver import FootnoteResolver
 from api.analysis.pdf_scanner import PDFScanner
-from api.web.web_page_provider import WebPageProvider
+from api.analysis.enricher import Enricher
 
 print("Training Classifiers...")
 trainer = DataProvider(dataset_name="BA", data_dir="./data/")
-line_svm = trainer.get_trained_svm(type="lines", retrain=False, balance_ratio=4, reload_data=False, run_test=False)
-char_svm = trainer.get_trained_svm(type="chars", retrain=False, balance_ratio=3, reload_data=False, run_test=False)
+line_svm = trainer.get_trained_svm(
+    type="lines", retrain=False, balance_ratio=4, reload_data=False, run_test=False)
+char_svm = trainer.get_trained_svm(
+    type="chars", retrain=False, balance_ratio=3, reload_data=False, run_test=False)
 
 resolver = FootnoteResolver(trainer)
 path = "../../../container_data/data/CELEX_32020R0740_EN_TXT.pdf"
@@ -21,55 +21,29 @@ scanner = PDFScanner()
 print("Processing pdf...")
 raw_blocks = scanner.pdf_to_blocks(path)
 
-with open("./log.txt", "w", encoding="utf-8") as f:
-    f.write(str(raw_blocks))
+# with open("./log.txt", "w", encoding="utf-8") as f:
+#     f.write(str(raw_blocks))
 
 print("Resolving footnotes...")
 tuples_by_page = resolver.resolve_footnotes(path, line_svm, char_svm)
 
-print("Enriching footnotes...")
-page_provider = WebPageProvider()
-kw_extractor = yake.KeywordExtractor()
-for page_tuples in tuples_by_page.items():
-    page_tuples = page_tuples[1]
-    for tuple in page_tuples:
-        footnote_line = tuple[0]
-        legal_act_text = page_provider.get_text_from_footnote(footnote_line.text)
-        if legal_act_text is not None:
-            ### keyword version
-            # keywords = kw_extractor.extract_keywords(legal_act_text)
-            # keyword_text = " ".join([keyword[0] for keyword in keywords])
-            # footnote_line.text = keyword_text
-            ### BM25 version
-            corpus = legal_act_text.split("\n\n")
-            tokenized_corpus = [doc.split(" ") for doc in corpus]
-            bm25 = BM25Okapi(tokenized_corpus)
-            tokenized_query = footnote_line.text.split(" ")
-            print("-------------")
-            print(footnote_line.text)
-            found = bm25.get_top_n(tokenized_query, corpus, n=1)[0]
-            print()
-            print(found)
-            footnote_line.text = found
+print("Enriching Footnotes...")
+enricher = Enricher()
+keyword_enriched = enricher.enrich(dict(tuples_by_page), mode="kw")
+bm25_enriched = enricher.enrich(dict(tuples_by_page), mode="bm25")
+summary_enriched = enricher.enrich(dict(tuples_by_page), mode="summary")
 
-enriched_blocks = resolver.insert_footnotes(raw_blocks, tuples_by_page)
-with open("./log.txt", "a", encoding="utf-8") as f:
-    f.write("\n\n")
-    f.write(str(enriched_blocks))
-
-# for page, page_list in tuples_by_page.items():
-#     for tup in page_list:
-#         print(tup[0].text)
-#         print("<>")
-#         print(tup[1])
-#         print("--------------")
-
+inserted_enriched = resolver.insert_footnotes(raw_blocks, tuples_by_page)
+# with open("./log.txt", "a", encoding="utf-8") as f:
+#     f.write("\n\n")
+#     f.write(str(enriched_blocks))
 
 
 def tsneplot(lines, embeddings, footnote_label_id):
     embs = np.empty((0, 8), dtype="f")
     word_labels = [line.text[:10] for line in lines]
-    color_list = ["green" if line.label == footnote_label_id else "red" for line in lines]
+    color_list = ["green" if line.label ==
+                  footnote_label_id else "red" for line in lines]
 
     # adds the vector for each of the closest words to the array
     for emb in embeddings:
@@ -77,25 +51,25 @@ def tsneplot(lines, embeddings, footnote_label_id):
 
     np.set_printoptions(suppress=True)
     Y = TSNE(n_components=2, learning_rate=200, random_state=42,
-                perplexity=len(lines) - 5, init="random").fit_transform(embs)
+             perplexity=len(lines) - 5, init="random").fit_transform(embs)
 
     # sets everything up to plot
     df = pd.DataFrame({"x": [x for x in Y[:, 0]],
-                        "y": [y for y in Y[:, 1]],
-                        "words": word_labels,
-                        "color": color_list})
+                       "y": [y for y in Y[:, 1]],
+                       "words": word_labels,
+                       "color": color_list})
 
     fig, _ = plt.subplots()
     fig.set_size_inches(10, 10)
 
     # basic plot
     p1 = sns.regplot(data=df,
-                        x="x",
-                        y="y",
-                        fit_reg=False,
-                        marker="o",
-                        scatter_kws={"s": 40, "facecolors": df["color"]}
-                        )
+                     x="x",
+                     y="y",
+                     fit_reg=False,
+                     marker="o",
+                     scatter_kws={"s": 40, "facecolors": df["color"]}
+                     )
 
     # adds annotations one by one with a loop
     for line in range(0, df.shape[0]):
---
+++ api/api/analysis/enricher.py
@@ -0,0 +1,47 @@
+import yake
+from rank_bm25 import BM25Okapi
+import torch
+from transformers import AutoTokenizer, AutoModelWithLMHead
+from api.web.web_page_provider import WebPageProvider
+
+class Enricher:
+
+    def __init__(self):
+        print("Enriching footnotes...")
+        self.page_provider = WebPageProvider()
+        self.kw_extractor = yake.KeywordExtractor()
+        self.t5_tokenizer = AutoTokenizer.from_pretrained('t5-base')
+        self.t5 = AutoModelWithLMHead.from_pretrained('t5-base', return_dict=True)
+    
+    def enrich(self, footnote_by_page, mode="kw"):
+        for page_tuples in footnote_by_page.items():
+            page_tuples = page_tuples[1]
+            for tuple in page_tuples:
+                footnote_line = tuple[0]
+                legal_act_text = self.page_provider.get_text_from_footnote(
+                    footnote_line.text, max_considerations=10)
+                if legal_act_text is not None:
+                    ### summary t5
+                    if mode == "summary":
+                        inputs = self.t5_tokenizer.encode("summarize: " + legal_act_text,
+                                                return_tensors='pt',
+                                                max_length=512,
+                                                truncation=True)
+                        summary_ids = self.t5.generate(inputs, max_length=150, min_length=80, length_penalty=5., num_beams=2)
+                        summary = self.t5_tokenizer.decode(summary_ids[0])
+                        if summary is not None:
+                            footnote_line.text = summary
+                    ### keyword version
+                    if mode == "kw":
+                        keywords = self.kw_extractor.extract_keywords(legal_act_text)
+                        keyword_text = " ".join([keyword[0] for keyword in keywords])
+                        footnote_line.text = keyword_text
+                    ### BM25 version
+                    if mode == "bm25":
+                        corpus = legal_act_text.split("\n\n")
+                        tokenized_corpus = [doc.split(" ") for doc in corpus]
+                        bm25 = BM25Okapi(tokenized_corpus)
+                        tokenized_query = footnote_line.text.split(" ")
+                        found = bm25.get_top_n(tokenized_query, corpus, n=1)[0]
+                        footnote_line.text = found
+        return footnote_by_page
